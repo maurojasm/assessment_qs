ce5986b1e967f3ff7fe65ca4e1109ab93813c715
Mauricio Rojas <79242002+maurojasm@users.noreply.github.com>
2023-10-23 17:55:16 -0700

    rm_dupl works

--- c++/rm_dupl.cpp
+++ c++/rm_dupl.cpp
@@ -12,25 +12,24 @@ void print(vector<int> nums) {
 }
 
 int my_removeDuplicates(vector<int>& nums) {
-    int k = 0, index = 0;
-    for(unsigned int i = 0; i < nums.size(); i++) {
-        while(i + index < nums.size()) {
-            if(nums[i] != nums[index]) {
-                int temp = nums[i];
-                nums[i] = nums[index];
-                nums[index] = temp;
-                k++;
-            } else {
-                index++;
+    int k = 0, index = 1;
+    for(unsigned int j = index; j < nums.size(); j++) {
+        if(nums[k] != nums[j]) {
+            index = j + 1;
+            if((k + 1) != j && (k + 1) < nums.size()) {
+                int temp = nums[k + 1];
+                nums[k + 1] = nums[j];
+                nums[j] = temp;
             }
+            k++;
         }
     }
 
-    return k;
+    return k + 1;
 }   
 
 int main() {
-    vector<int> nums1{1, 1, 2}; 
+    vector<int> nums1{1, 2, 3, 4, 5}; 
     vector<int> nums2{0,0,1,1,1,2,2,3,3,4};
     print(nums1);
     std::cout << "k = " << my_removeDuplicates(nums1) << '\n';
